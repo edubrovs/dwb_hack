648997d4b9daacf5c499eb768fa28700329590e3
edubrovs <ethel.dubrovsky>
2017-03-11 18:56:28 -0500

    style

--- data.js
+++ data.js
@@ -18,6 +18,18 @@ function searchData (arg) {
   event.preventDefault(); 
   var input = $('.form-control').val()
   console.log('search arg', input)
+  var dataFound = dataMap[tickerMap[input]] || dataMap[nameMap[input]]
+  console.log('dataFound',dataFound)
+
+  if (dataFound) {
+    $('.table_ticker').text(dataFound.ticker)
+    $('.table_name').text(dataFound.name)    
+    if (dataFound.type === 'GSK') {
+      $('.table_gsk').text(dataFound.expense_ratio)
+    } else {
+      $('.table_pfe').text(dataFound.expense_ratio)
+    }
+  }
 }
 
 function loadTickerData() {
@@ -32,6 +44,7 @@ function loadTickerData() {
         for (k = 0; k < file.length; k++) {
           uuid++;
           entry = file[k];
+          entry.type = (type.indexOf('GSK') !== -1) ? 'GSK' : 'PFE'
           dataMap[uuid] = entry;
           nameMap[entry.name] = uuid;
           if (entry.ticker) {
